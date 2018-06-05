a6b3a5d570c1dc1a087b5f645c806e68683e14d1
OSCAR ANDRES CESPEDES DIAZ <oscar.cespedes@correo.policia.gov.co>
2018-06-05 10:35:20 -0500

    Proyecto Servicios WEB Compañia Galletas

---
+++ CookieCompany.Domain.Services/IProductsService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CookieCompany.Domain.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IProductsService" in both code and config file together.
+    [ServiceContract]
+    public interface IProductsService
+    {
+        [OperationContract]
+        IEnumerable<string> GetProducts();
+
+        [OperationContract]
+        string GetProductsById(int id);
+    }
+}
---
+++ CookieCompany.Domain.Services/ProductsService.svc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CookieCompany.Domain.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ProductsService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ProductsService.svc or ProductsService.svc.cs at the Solution Explorer and start debugging.
+    public class ProductsService : IProductsService
+    {
+        readonly Model.Context.CookieCompanyModel model;
+
+        public ProductsService()
+        {
+            model = new Model.Context.CookieCompanyModel();
+        }
+
+
+        public IEnumerable<string> GetProducts()
+        {
+            return model.Producto.Select(x => x.Name);
+        }
+
+        public string GetProductsById(int id) => model.Producto.Find(id).Name;
+    }
+}
